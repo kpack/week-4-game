55bfbe9bac68577444e86459132aaea2fcee20b3
kpack <kevinpackler@gmail.com>
2017-06-14 08:52:19 -0400

    change

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+// var targetscore = "0"
+var targetscore = math.floor((Math.random() * 120) + 19);
+var redcrystal = math.floor((Math.random() * 12) + 1);
+var bluecrystal = math.floor((Math.random() * 12) + 1);
+var greencrystal = math.floor((Math.random() * 12) + 1);
+var yellowcrystal = math.floor((Math.random() * 12) + 1);
+var yourscore = 0
+var wincount = 0
+var losscount = 0
+
+// reset yourscore
+// random target score
+
+
+$("bluecrystal").click();((
+
+
+random crystal for each color between 1-12
+onclick each crystal + value to yourscore
+if yourscore > alert you lost
+	add 1 to loss count
+	reset your score to zero
+	random target score
+	random crystal scores 1-12
+
+if your score = random score, alert you win
+	add 1 to win
+	reset your score to zero
+	random target score
+	random crystal scores 1-12
