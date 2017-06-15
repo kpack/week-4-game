d865a535f631358949368d03ee2847ca583c2404
kpack <kevinpackler@gmail.com>
2017-06-15 10:21:37 -0400

    change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,11 @@
 
 
 // set variables
-var targetScore = math.floor((Math.random() * 120) + 19);
-var red = math.floor((Math.random() * 12) + 1);
-var blue = math.floor((Math.random() * 12) + 1);
-var green = math.floor((Math.random() * 12) + 1);
-var yellow = math.floor((Math.random() * 12) + 1);
+var targetScore = Math.floor((Math.random() * 120) + 19);
+var red = Math.floor((Math.random() * 12) + 1);
+var blue = Math.floor((Math.random() * 12) + 1);
+var green = Math.floor((Math.random() * 12) + 1);
+var yellow = Math.floor((Math.random() * 12) + 1);
 var yourScore = 0;
 var winCount = 0;
 var lossCount = 0;
@@ -31,17 +31,7 @@ alert ("You lose!");
   reset()
 }
 
-//reset game
 
-function reset(){
-	targetScore = math.floor((Math.random() * 120) + 19);
-	$('#targetscore').text(targetScore);
-	red = math.floor((Math.random() * 12) + 1);
-	blue = math.floor((Math.random() * 12) + 1);
-	green = math.floor((Math.random() * 12) + 1);
-	yellow = math.floor((Math.random() * 12) + 1);
-	yourScore = 0;
-}
 
 // crystal click functions
 
@@ -68,8 +58,7 @@ function reset(){
         else if ( userTotal > Random){
           loser();
         } 
-  })  
-
+   })  
 
  $('#yellow').on ('click', function(){
     yourScore = yourScore + yellow;
@@ -98,19 +87,16 @@ function reset(){
 
 
 
+//reset game
+
+function reset(){
+	targetScore = math.floor((Math.random() * 120) + 19);
+	$('#targetscore').text(targetScore);
+	red = math.floor((Math.random() * 12) + 1);
+	blue = math.floor((Math.random() * 12) + 1);
+	green = math.floor((Math.random() * 12) + 1);
+	yellow = math.floor((Math.random() * 12) + 1);
+	yourScore = 0;
+}
 
 
-
-random crystal for each color between 1-12
-onclick each crystal + value to yourscore
-if yourscore > alert you lost
-	add 1 to loss count
-	reset your score to zero
-	random target score
-	random crystal scores 1-12
-
-if your score = random score, alert you win
-	add 1 to win
-	reset your score to zero
-	random target score
-	random crystal scores 1-12
